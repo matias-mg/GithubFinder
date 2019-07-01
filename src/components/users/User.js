d0c2fcb97661a623b61969b69739876a25ad392a
Chross <matia.medinag@alumnos.duoc.cl>
2019-06-30 23:06:02 -0400

    User component refactored with Hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,108 +1,108 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired
-  };
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    company,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user;
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      company,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable
-    } = this.props.user;
+  if (loading) return <Spinner />;
 
-    if (this.props.loading) return <Spinner />;
-
-    return (
-      <React.Fragment>
-        <Link to='/' className='btn btn-light'>
-          Back to Search
+  return (
+    <React.Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back to Search
         </Link>
-        Hireable:{' '}
-        {hireable ? (
-          <i className='fas fa-check text-success' />
-        ) : (
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
           <i className='fas fa-times-circle text-danger' />
         )}
-        <div className='card grid-2'>
-          <div className='all-center'>
-            <img
-              src={avatar_url}
-              className='round-img'
-              style={{ width: '150px' }}
-              alt='avatar img'
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <React.Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </React.Fragment>
-            )}
-            <a href={html_url} className='btn btn-dark my-1'>
-              Visit Github Profile
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            style={{ width: '150px' }}
+            alt='avatar img'
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <React.Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </React.Fragment>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            Visit Github Profile
             </a>
-            <ul>
-              <li>
-                <strong>Username: </strong>
-                {login}
-              </li>
-              <li>
-                {company && (
-                  <React.Fragment>
-                    <strong>Company: </strong>
-                    {company}
-                  </React.Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <React.Fragment>
-                    <strong>Blog: </strong>
-                    {blog}
-                  </React.Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
+          <ul>
+            <li>
+              <strong>Username: </strong>
+              {login}
+            </li>
+            <li>
+              {company && (
+                <React.Fragment>
+                  <strong>Company: </strong>
+                  {company}
+                </React.Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <React.Fragment>
+                  <strong>Blog: </strong>
+                  {blog}
+                </React.Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-succes">Following: {following}</div>
-          <div className="badge badge-light">Public Repos: {public_repos}</div>
-          <div className="badge badge-dark">Public Gists: {public_gists}</div>
-        </div>
-        <Repos repos={this.props.repos} />
-      </React.Fragment>
-    );
-  }
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-succes">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </React.Fragment>
+  );
+
 }
+
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired
+};
 
 export default User;
